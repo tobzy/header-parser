f28e408f7e8d541d7c69c51e35363f6d39d785fa
methodmain <onuegbu.tobechukwu@yahoo.com>
2017-01-04 16:50:29 +0000

    thsi is almost d final

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var express = require("express");
 var app = express();
 
 app.get('/', function (req, res) {
+    
+    var re = /\([^)(]*\)/;
+    
     var data = {
-        ipaddress: req.ip,
-        language: req.headers['accept-language'],
-        software: req.headers['user-agent']
+        ipaddress: req.headers['x-forwarded-for'] || req.ip,
+        language: req.headers['accept-language'].split(',')[0],
+        software: re.exec(req.headers['user-agent'])[0].slice(1,-1)
     }
     
     res.send(data);
